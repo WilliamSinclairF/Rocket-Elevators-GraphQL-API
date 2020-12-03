74bfc635756023eb2d60689e30b88abb3aefaf26
WilliamSinclairF <willzy@outlook.com>
2020-12-03 18:58:38 -0500

    trying to deploy

--- src/index.ts
+++ src/index.ts
@@ -8,10 +8,41 @@ import { InterventionResolvers } from './resolvers/InterventionResolvers';
 
 (async () => {
   const app = express();
-
-  require('dotenv').config({ path: __dirname + '/.env' });
-
-  await createConnections();
+  await createConnections([
+  {
+    name: 'default',
+    type: 'mysql',
+    host: process.env.MYSQLHOST || 'localhost',
+    port: 3306,
+    username: process.env.MYSQLUSER || 'root',
+    password: process.env.MYSQLPW || 'root',
+    database: process.env.MYSQLDB || 'app_development',
+    synchronize: false,
+    logging: true,
+    entities: [
+      process.env.NODE_ENV === 'development'
+        ? './src/entities/MySQL/*.*'
+        : './dist/entities/MySQL/*.js',
+    ],
+  },
+  {
+    name: 'pg',
+    type: 'postgres',
+    host: process.env.PGHOST || 'localhost',
+    port: 5432,
+    username: process.env.PGUSER || 'william',
+    password: process.env.PGPW || 'root',
+    database: process.env.PGDB || 'dwh_development',
+    schema: 'public',
+    synchronize: false,
+    logging: true,
+    entities: [
+      process.env.NODE_ENV === 'development'
+        ? './src/entities/PG/*.*'
+        : './dist/entities/PG/*.js',
+    ],
+  },
+]);
 
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
